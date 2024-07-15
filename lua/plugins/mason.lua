b4c1613987fb8a734851dce88356f4022a1b4bc8
Hundera Awoke <hunderaweke@gmail.com>
2024-07-15 11:37:33 +0300

    added: python lsp servers for insured installation

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,5 +1,3 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- Customize Mason plugins
 
 ---@type LazySpec
@@ -11,6 +9,7 @@ return {
     opts = {
       ensure_installed = {
         "lua_ls",
+        "pylsp",
         -- add more arguments for adding more language servers
       },
     },
@@ -22,6 +21,7 @@ return {
     opts = {
       ensure_installed = {
         "stylua",
+        "pylsp",
         -- add more arguments for adding more null-ls sources
       },
     },
